f531d3fb408c42f7903831aba54342f66a4ff46e
murillobazz <murillobazilio@gmail.com>
2023-06-26 19:44:31 -0300

    exercises 4.9 - 4.10

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -1,17 +1,69 @@
 const mongoose = require('mongoose'); // Import mongoose to connect to the DB
 const supertest = require('supertest'); // Import supertest to make test API calls
 const app = require('../app'); // Import our Express application to be tested
+const Blog = require('../models/blog');
 
 const api = supertest(app); // Wraps the application with the supertest, making it a superagent object
 // Now, tests can use this superagent object to make HTTP requests to the backend.
+
+const initialBlogs = [
+  {
+    title: 'First Blog',
+    author: 'First Author',
+    url: 'firstURL',
+    likes: 1
+  },
+  {
+    title: 'Second Blog',
+    author: 'Second Author',
+    url: 'secondURL',
+    likes: 2
+  }
+];
+// Reinitialize the database with 'initialBlogs' before every test.
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
+})
 
 test('the blogs are returned as json', async () => {
   const response = await api.get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/);
-    
+
     console.log(response.body);
 })
+
+test('the id property exists', async () => {
+  const response = await api.get('/api/blogs');
+  const blogs = response.body;
+
+  expect(blogs[0].id).toBeDefined();
+  console.log(blogs);
+})
+
+test('succesfully creates a new blog', async () => {
+  const requestBody = {
+    title: 'apiTest',
+    author: 'apiTestAuthor',
+    url: 'apiTestURL',
+    likes: 1
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(requestBody)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+  
+  const response = await api.get('/api/blogs');
+
+  expect(response.body.length).toEqual(initialBlogs.length + 1);
+})
+
 // The afterAll method (from jest) used to run code (close the db connection) after running all tests.
 afterAll(async () => {
   await mongoose.connection.close();
